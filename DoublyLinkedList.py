b9b58be630db2e7226927816007de0c91d4ef90e
Saumya Saxena <saumyasaxena004@gmail.com>
2020-01-04 21:08:08 -0800

    Add files via upload

---
+++ DoublyLinkedList.py
@@ -0,0 +1,15 @@
+class Node(object):
+
+    def __init__(self,item):
+        self.item = item
+        self.next_node = None
+        self.prev_node = None
+
+a = Node(1)
+b = Node(2)
+c = Node(3)
+
+a.next_node = b
+b.prev_node = a
+b.next_node = c
+c.prev_node = b
